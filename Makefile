3fcd75efffd256fad50cf938aa2bc44372631920
Andrew Harvey <andrew.harvey4@gmail.com>
2018-04-21 15:58:16 +1000

    add diff tool

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
 download:
 	wget -O nsw-public-schools.json 'https://data.cese.nsw.gov.au/data/dataset/027493b2-33ad-3f5b-8ed9-37cdca2b8650/resource/af20d17c-a7ac-4251-af75-e5ae66573e92/download/collections.json'
+
+toOSM:
+	./index.js
+
+getFromOverpass:
+	wget -O osm-schools.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0Aarea%5B%22ISO3166-2%22%3D%22AU-NSW%22%5D%5Bboundary%3Dadministrative%5D-%3E.searchArea%3B%0A%28%0A%20%20node%5B%22amenity%22%3D%22school%22%5D%28area.searchArea%29%3B%0A%20%20way%5B%22amenity%22%3D%22school%22%5D%28area.searchArea%29%3B%0A%20%20relation%5B%22amenity%22%3D%22school%22%5D%28area.searchArea%29%3B%0A%29%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B'
+	./node_modules/.bin/osmtogeojson osm-schools.json > osm-schools.geojson
+
+diff:
+	./diff.js -o matches.json osm-schools.geojson nsw-public-schools.geojson
---
+++ diff.js
@@ -0,0 +1,145 @@
+#!/usr/bin/env node
+
+/*
+ * Compares two GeoJSON files,
+ *   - A current state OSM and,
+ *   - B reference dataset
+ *
+ * The assumptions are that B consists of only Points with A any geometry type.
+ */
+
+const fs = require('fs');
+
+const argv = require('minimist')(process.argv.slice(2));
+const _ = require('lodash');
+const turf = {
+    featureEach: require('@turf/meta').featureEach,
+    featureCollection: require('@turf/helpers').featureCollection,
+    bbox: require('@turf/bbox').default
+};
+const rbush = require('rbush');
+const knn = require('rbush-knn');
+const kdbush = require('kdbush');
+const geokdbush = require('geokdbush');
+const whichPolygon = require('which-polygon');
+
+if (argv._.length !== 2 || !argv.o) {
+    console.log("Usage: $0 [--tolerance 200] -o diff.geojson a.geojson b.geojson");
+}
+
+const filepathA = argv._[0];
+const filepathB = argv._[1];
+const matchesFilepath = argv.o;
+
+/* maximum distance between pointns to consider for a match in meters */
+const tolerance = argv.tolerance || 200;
+
+// load GeoJSON files
+const a = JSON.parse(fs.readFileSync(filepathA));
+const b = JSON.parse(fs.readFileSync(filepathB));
+
+// index A by id
+const aFeaturesByID = {};
+
+// ensure the ID field is populated in A
+a.features = a.features.map((feature, index) => {
+    if (!feature.id)
+        feature.id = index;
+
+    // add feature to the index of A by id
+    aFeaturesByID[feature.id] = feature;
+    return feature;
+});
+
+// ensure the ID field is populated in B
+b.features = b.features.map((feature, index) => {
+    if (!feature.id)
+        feature.id = index;
+    return feature;
+});
+
+
+// index of Points in A
+const aPointsIndex = kdbush(a.features.filter((feature) => {
+    return feature.geometry && feature.geometry.type == 'Point';
+}),
+    (feature) => { return feature.geometry.coordinates[0]; },
+    (feature) => { return feature.geometry.coordinates[1]; });
+
+// index of Polygons in A for strictly point in polygon queries
+const aPolygonsIndex = whichPolygon(a);
+
+// index of LineStrings and Polygons in A for k nearest neigbour queries
+const aPolygonsKnnIndex = rbush().load(a.features.filter((feature) => {
+    return feature.geometry && feature.geometry.type !== 'Point';
+}).map((feature) => {
+    const bbox = turf.bbox(feature);
+    return {
+        minX: bbox[0],
+        minY: bbox[1],
+        maxX: bbox[2],
+        maxY: bbox[3],
+        id: feature.id
+    };
+}));
+
+/* B -> A
+ * {
+ *   b.id: { from: b, to: [As] }
+ * }
+ * */
+var matches = {};
+
+// for each point in B...
+turf.featureEach(b, (bFeature) => {
+    const bLng = bFeature.geometry.coordinates[0];
+    const bLat = bFeature.geometry.coordinates[1];
+
+    // find nearest point from A
+    const nearbyPoints = geokdbush.around(aPointsIndex, bLng, bLat, 1, tolerance / 1000);
+    if (nearbyPoints.length) {
+        const nearbyPoint = nearbyPoints[0];
+        if (!matches[bFeature.id]) {
+            matches[bFeature.id] = { from: bFeature, to: [] };
+        }
+        matches[bFeature.id].to.push(nearbyPoint);
+    }
+
+    // find polygons from A which B is within
+    const insidePolygons = aPolygonsIndex([bLng, bLat], true);
+    if (insidePolygons && insidePolygons.length) {
+        if (!matches[bFeature.id]) {
+            matches[bFeature.id] = { from: bFeature, to: [] };
+        }
+        insidePolygons.forEach((insidePolygon) => {
+            matches[bFeature.id].to.push(aFeaturesByID[insidePolygon.id]);
+        });
+    } else {
+        const nearestPolygons = knn(aPolygonsKnnIndex, bLng, bLat, 1, null, tolerance / 1000);
+        if (nearestPolygons && nearestPolygons.length) {
+            const nearestPolygon = nearestPolygons[0];
+
+            if (!matches[bFeature.id]) {
+                matches[bFeature.id] = { from: bFeature, to: [] };
+            }
+
+            matches[bFeature.id].to.push(aFeaturesByID[nearestPolygon.id]);
+        }
+    }
+});
+
+const unmatchedFromA = a.features.filter((feature) => { return !(feature.id in _.flatten(Object.values(matches)).map((object) => { return object.id; })) });
+const unmatchedFromB = b.features.filter((feature) => { return !(feature.id in matches); });
+
+console.log(`Total features from A: ${a.features.length}`);
+console.log(`Total features from B: ${b.features.length}`);
+console.log(`Total features matched from A: ${a.features.length - unmatchedFromA.length}`);
+console.log(`Total features matched from B: ${Object.keys(matches).length}`);
+console.log(`Features from A not found in B: ${unmatchedFromA.length}`);
+console.log(`Features from B not found in A: ${unmatchedFromB.length}`);
+
+
+fs.writeFileSync(matchesFilepath, JSON.stringify(matches, null, 2));
+
+fs.writeFileSync('aNotinB.geojson', JSON.stringify(turf.featureCollection(unmatchedFromA), null, 2));
+fs.writeFileSync('bNotinA.geojson', JSON.stringify(turf.featureCollection(unmatchedFromB), null, 2));
